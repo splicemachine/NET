d5ca95fd693eeb04fe428b3a80732c679dec01ee
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2020-07-29 13:19:08 +0200

    Issue #9 - converted the 'switch' statement inside the 'StreamExtensions' class into the `switch` expression for brevity.

--- SpliceMachine.Drda/Extensions/StreamExtensions.cs
+++ SpliceMachine.Drda/Extensions/StreamExtensions.cs
@@ -4,6 +4,7 @@ using System.IO;
 namespace SpliceMachine.Drda
 {
     using static System.Diagnostics.Trace;
+    using static CodePoint;
 
     internal static class StreamExtensions
     {
@@ -47,68 +48,29 @@ namespace SpliceMachine.Drda
 
             TraceInformation($"RCID: {response.RequestCorrelationId}, CP: {response.Command.CodePoint}");
 
-            switch (response.Command.CodePoint)
+            return response.Command.CodePoint switch
             {
-                case CodePoint.EXSATRM:
-                    return new ExchangeServerAttributesResponse(response);
-
-                case CodePoint.ACCSECRM:
-                    return new AccessSecurityDataResponse(response);
-
-                case CodePoint.SECCHKRM:
-                    return new SecurityCheckResponse(response);
-
-                case CodePoint.ACCRDBRM:
-                    return new AccessRelationalDatabaseResponse(response);
-
-                case CodePoint.CMDCHKRM:
-                    return new CommandCheckResponse(response);
-
-                case CodePoint.SQLERRRM:
-                    return new SqlErrorResponse(response);
-
-                case CodePoint.RDBUPDRM:
-                    return new RelationalDatabaseUpdateResponse(response);
-
-                case CodePoint.PBSD:
-                    return new PiggyBackSchemaDescResponse(response);
-
-                case CodePoint.SQLCARD:
-                    return new CommAreaRowDescResponse(response);
-
-                case CodePoint.SQLDARD:
-                    return new DescAreaRowDescResponse(response);
-
-                case CodePoint.SYNTAXRM:
-                    return new SyntaxResponse(response);
-
-                case CodePoint.RSLSETRM:
-                    return new RelationalDatabaseResultSetResponse(response);
-
-                case CodePoint.SQLRSLRD:
-                    return new SqlResultSetDataResponse(response);
-
-                case CodePoint.OPNQRYRM:
-                    return new OpenQueryCompleteResponse(response);
-
-                case CodePoint.SQLCINRD:
-                    return new SqlResultSetColumnInfoResponse(response);
-
-                case CodePoint.QRYDSC:
-                    return new QueryAnswerSetDescResponse(response);
-
-                case CodePoint.QRYDTA:
-                    return new QueryAnswerSetDataResponse(response);
-
-                case CodePoint.EXTDTA:
-                    return new QueryAnswerSetExtraDataResponse(response);
-
-                case CodePoint.ENDUOWRM:
-                    return new EndUnitOfWorkResponse(response);
-
-                default:
-                    throw new InvalidOperationException();
-            }
+                PBSD => new PiggyBackSchemaDescResponse(response),
+                QRYDSC => new QueryAnswerSetDescResponse(response),
+                QRYDTA => new QueryAnswerSetDataResponse(response),
+                EXTDTA => new QueryAnswerSetExtraDataResponse(response),
+                SQLCARD => new CommAreaRowDescResponse(response),
+                SQLDARD => new DescAreaRowDescResponse(response),
+                EXSATRM => new ExchangeServerAttributesResponse(response),
+                ACCSECRM => new AccessSecurityDataResponse(response),
+                SECCHKRM => new SecurityCheckResponse(response),
+                ACCRDBRM => new AccessRelationalDatabaseResponse(response),
+                CMDCHKRM => new CommandCheckResponse(response),
+                SQLERRRM => new SqlErrorResponse(response),
+                RDBUPDRM => new RelationalDatabaseUpdateResponse(response),
+                SYNTAXRM => new SyntaxResponse(response),
+                RSLSETRM => new RelationalDatabaseResultSetResponse(response),
+                SQLRSLRD => new SqlResultSetDataResponse(response),
+                OPNQRYRM => new OpenQueryCompleteResponse(response),
+                SQLCINRD => new SqlResultSetColumnInfoResponse(response),
+                ENDUOWRM => new EndUnitOfWorkResponse(response),
+                _ => throw new InvalidOperationException()
+            };
         }
     }
 }
