9803f6352ca5906ef65c231a00b4f64c228dac90
saravanandevaraj <saravanan.devaraj@ideas2it.com>
2021-03-03 15:26:21 +0530

    All data types test function added

--- SpliceMachine.IntegrationTests/AdoNetTest.cs
+++ SpliceMachine.IntegrationTests/AdoNetTest.cs
@@ -125,50 +125,6 @@ namespace SpliceMachine.IntegrationTests
 
         #endregion
 
-        #region DML SELECT
-
-        [TestMethod]
-        public void TestDML()
-        {
-            const string DmlCreateQuery = "CREATE TABLE TestTable(Col1 BIGINT,Col2 BLOB,Col3 BOOLEAN,Col4 CHAR(2), Col5 CLOB(6535),Col6 DATE,Col7 DECIMAL,Col8 DOUBLE,Col9 FLOAT,Col10 INT,Col11 NUMERIC, Col12 REAL, Col13 SMALLINT,Col15 TIME, Col16 TIMESTAMP, Col17 TINYINT, Col18 VARCHAR(200))";
-            const string DmlInsertQuery = "INSERT INTO TestTable VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
-            
-            using (var connection = new SpliceDbConnection())
-            {
-                connection.ConnectionString = "uid=" + userName + ";pwd=" + password + ";host=" + hostName + ";port=" + port + "";
-                connection.Open();
-                using (var command = connection.CreateCommand())
-                {
-                    command.CommandText = DmlCreateQuery;
-                    var result = command.ExecuteNonQuery();
-                    command.CommandText = DmlInsertQuery;
-                    var blobSample = new byte[] { 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20 };
-                    command.Parameters.Add(new SpliceDbParameter() { Value = 10 });
-                    command.Parameters.Add(new SpliceDbParameter() { Value = blobSample });
-                    command.Parameters.Add(new SpliceDbParameter() { Value = true });
-                    command.Parameters.Add(new SpliceDbParameter() { Value = 'A' });
-                    command.Parameters.Add(new SpliceDbParameter() { Value = "TUlNRS1WZXJzaW9uOiAxLjANClgtTWFpbGVyOiBNYWlsQmVlLk5FVCA4LjAuNC40MjgNClN1YmplY3Q6IHRlc3Qgc3ViamVjdA0KVG86IGtldmlubUBkYXRhbW90aW9uLmNvbQ0KQ29udGVudC1UeXBlOiBtdWx0aXBhcnQvYWx0ZXJuYXRpdmU7DQoJYm91bmRhcnk9Ii0tLS09X05leHRQYXJ0XzAwMF9BRTZCXzcyNUUwOUFGLjg4QjdGOTM0Ig0KDQoNCi0tLS0tLT1fTmV4dFBhcnRfMDAwX0FFNkJfNzI1RTA5QUYuODhCN0Y5MzQNCkNvbnRlbnQtVHlwZTogdGV4dC9wbGFpbjsNCgljaGFyc2V0PSJ1dGYtOCINCkNvbnRlbnQtVHJhbnNmZXItRW5jb2Rpbmc6IHF1b3RlZC1wcmludGFibGUNCg0KdGVzdCBib2R5DQotLS0tLS09X05leHRQYXJ0XzAwMF9BRTZCXzcyNUUwOUFGLjg4QjdGOTM0DQpDb250ZW50LVR5cGU6IHRleHQvaHRtbDsNCgljaGFyc2V0PSJ1dGYtOCINCkNvbnRlbnQtVHJhbnNmZXItRW5jb2Rpbmc6IHF1b3RlZC1wcmludGFibGUNCg0KPHByZT50ZXN0IGJvZHk8L3ByZT4NCi0tLS0tLT1fTmV4dFBhcnRfMDAwX0FFNkJfNzI1RTA5QUYuODhCN0Y5MzQtLQ0K" });
-                    command.Parameters.Add(new SpliceDbParameter() { Value = "02/16/2021" });
-                    command.Parameters.Add(new SpliceDbParameter() { Value = 2.1 });
-                    command.Parameters.Add(new SpliceDbParameter() { Value = 2.2 });
-                    command.Parameters.Add(new SpliceDbParameter() { Value = 2.3 });
-                    command.Parameters.Add(new SpliceDbParameter() { Value = 5 });
-                    command.Parameters.Add(new SpliceDbParameter() { Value = 4 });
-                    command.Parameters.Add(new SpliceDbParameter() { Value = 6 });
-                    command.Parameters.Add(new SpliceDbParameter() { Value = 1 });
-                    command.Parameters.Add(new SpliceDbParameter() { Value = "TUlNRS1WZXJzaW9uOiAxLjANClgtTWFpbGVyOiBNYWlsQmVlLk5FVCA4LjAuNC40MjgNClN1YmplY3Q6IHRlc3Qgc3ViamVjdA0KVG86IGtldmlubUBkYXRhbW90aW9uLmNvbQ0KQ29udGVudC1UeXBlOiBtdWx0aXBhcnQvYWx0ZXJuYXRpdmU7DQoJYm91bmRhcnk9Ii0tLS09X05leHRQYXJ0XzAwMF9BRTZCXzcyNUUwOUFGLjg4QjdGOTM0Ig0KDQoNCi0tLS0tLT1fTmV4dFBhcnRfMDAwX0FFNkJfNzI1RTA5QUYuODhCN0Y5MzQNCkNvbnRlbnQtVHlwZTogdGV4dC9wbGFpbjsNCgljaGFyc2V0PSJ1dGYtOCINCkNvbnRlbnQtVHJhbnNmZXItRW5jb2Rpbmc6IHF1b3RlZC1wcmludGFibGUNCg0KdGVzdCBib2R5DQotLS0tLS09X05leHRQYXJ0XzAwMF9BRTZCXzcyNUUwOUFGLjg4QjdGOTM0DQpDb250ZW50LVR5cGU6IHRleHQvaHRtbDsNCgljaGFyc2V0PSJ1dGYtOCINCkNvbnRlbnQtVHJhbnNmZXItRW5jb2Rpbmc6IHF1b3RlZC1wcmludGFibGUNCg0KPHByZT50ZXN0IGJvZHk8L3ByZT4NCi0tLS0tLT1fTmV4dFBhcnRfMDAwX0FFNkJfNzI1RTA5QUYuODhCN0Y5MzQtLQ0K" });
-                    command.Parameters.Add(new SpliceDbParameter() { Value = "15:09:02" });
-                    command.Parameters.Add(new SpliceDbParameter() { Value = "02/16/2021" });
-                    command.Parameters.Add(new SpliceDbParameter() { Value = 1 });
-                    command.Parameters.Add(new SpliceDbParameter() { Value = "HELLO" });
-                    
-                }
-
-                connection.Close();
-            }
-        }
-        #endregion
-
         #region All data types test
 
         [TestMethod]
